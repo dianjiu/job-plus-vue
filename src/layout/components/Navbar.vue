a5d9f2b0e10afa80ee7c796dee0d690d6c157a6f
dianjiu <dianjiu@dianjiu.cc>
2021-07-28 11:23:46 +0800

    优化Navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,23 +7,26 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img src="@/assets/imgs/admin.jpg" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
-              Home
+              首页
             </el-dropdown-item>
           </router-link>
           <a target="_blank" href="https://github.com/dianjiu/job-plus-vue">
-            <el-dropdown-item>Github</el-dropdown-item>
+            <el-dropdown-item>源码</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://dianjiu.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+          <a target="_blank" href="https://jobplus.dianjiu.org.cn">
+            <el-dropdown-item>文档</el-dropdown-item>
+          </a>
+          <a target="_blank" href="https://dianjiu.co">
+            <el-dropdown-item>点九</el-dropdown-item>
           </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">登出</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
